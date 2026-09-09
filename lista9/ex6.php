fe45e75673deeec95c66ed90f3e0029d82a07948
miguellorenco <miguellorencodeoliveira@gmail.com>
2026-09-09 10:48:47 -0300

    Exercicio 9

---
+++ lista9/ex6.php
@@ -0,0 +1,14 @@
+<?php
+
+$musicas = ["All by Myself", "Run to You", "I Have Nothing", "Fix you", "My Heart Will Go On", "Super Trouper"];
+
+foreach ($musicas as $music) {
+    echo "$music <br>" ;
+
+}
+
+
+    $qtd = count ($musicas);
+echo "Quantidade de músicas: ", $qtd;
+
+?>
