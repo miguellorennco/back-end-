2246964a3dcbb0a505af980dfd36411d7a3ac770
miguellorenco <miguellorencodeoliveira@gmail.com>
2026-09-09 10:50:07 -0300

    Delete lista9 directory

--- lista9/ex2.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$alunos = ["Marlon", "Maria", "Ana", "Pedro"];
-
-// troque os indices 0, 1, 2, 3 e 4
-
-echo $alunos[2];
-?>
--- lista9/ex5.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$convidados = ["João", "Maria", "Lucas", "Gui"];
-
-$qtd = count ($convidados);
-
-echo "Quantidade de convidados: ", $qtd;
-
-?>
